a29804866e2cc1c36b79904a022b6b77ff877a52
Nabalrog <navarrogc_395@hotmail.com>
2020-12-12 20:41:05 -0500

    estaba bien

--- db/Data_db.py
+++ db/Data_db.py
@@ -11,8 +11,8 @@ database_data = Dict[str, DataInDB]
 
 
 def get_data(nombreGasto: str):
-    if nombreGasto in database_transactions.keys():
-        return database_transactions[nombreGasto]
+    if nombreGasto in database_data.keys():
+        return database_data[nombreGasto]
     else:
         return None
 
